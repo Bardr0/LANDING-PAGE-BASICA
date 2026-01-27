1c8ce2d9c40f0c219babd2ace64d80d3ed5084e2
Alejandro Barturen <alebartureng@gmail.com>
2026-01-27 11:14:25 -0500

    creacion del navar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+       <Navbar></Navbar>
+       <Hero></Hero>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Logo from "../../assets/Logo.png";
+
+const navbarlinks = [
+  {
+    id:1,
+    title: "Inicio",
+    link: "/"
+  },
+    {
+    id:2,
+    title: "Nosotros",
+    link: "#"
+  },
+  {
+    id:3,
+    title: "Contacto",
+    link: "#"
+  },
+  {
+    id:4,
+    title: "Soporte",
+    link: "#"
+  }
+]
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <div>
+          <img src={Logo} alt="Logo del sitio" className="w-[100px]" />
+        </div>
+
+        <div>
+          <ul>
+            {navbarlinks.map((link)=> (
+              <li key={link.id}>
+                <a href={link.link}>{link.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
